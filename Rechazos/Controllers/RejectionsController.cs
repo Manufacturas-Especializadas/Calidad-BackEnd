17668f779a599ef7fbc9c698d608b2195948e2b4
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2025-09-26 10:05:19 -0600

    Metodo para eliminar

--- Rechazos/Controllers/RejectionsController.cs
+++ Rechazos/Controllers/RejectionsController.cs
@@ -142,6 +142,24 @@ namespace Rechazos.Controllers
             return Ok(list);
         }
 
+        [HttpDelete]
+        [Route("Delete/{id:int}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var rejection = await _context.Rejections.FindAsync(id);
+
+            if (rejection == null) return NotFound("Rejection not found");
+
+            _context.Rejections.Remove(rejection);
+            await _context.SaveChangesAsync();
+
+            return Ok(new
+            {
+                success = true,
+                message = "Rejection deleted successfully"
+            });
+        }
+
         [HttpPost]
         [Route("Create")]
         public async Task<IActionResult> Create([FromForm] RejectionDto rejection)
